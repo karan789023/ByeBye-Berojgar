b8342a5bbdeb160ec3950a1db2f1729854813cb1
Karan <mybussion4@gmail.com>
2026-05-30 21:43:15 +0530

    fix api url

--- bye-bye-berojgar/src/pages/Testpage/index.jsx
+++ bye-bye-berojgar/src/pages/Testpage/index.jsx
@@ -3,6 +3,7 @@ import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 
 const Testpage = () => {
+    console.log("API URL:", import.meta.env.VITE_API_URL);
   const { id } = useParams();
   const navigate = useNavigate();
 
